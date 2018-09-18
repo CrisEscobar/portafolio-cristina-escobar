0ca69e6bcc7e970d5790601a194581ba97c411b4
CrisEscobar <crisescobar0@hotmail.com>
2018-09-17 20:23:22 -0500

    añadiendo secciones

--- app.js
+++ app.js
@@ -7,3 +7,23 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+
+// TESTIMONIALS
+// You can also use "$(window).load(function() {"
+$(function () {
+ // Slideshow 4
+$("#slider3").responsiveSlides({
+  auto: true,
+  pager: false,
+  nav: true,
+  speed: 1000,
+  namespace: "callbacks",
+  before: function () {
+$('.events').append("<li>before event fired.</li>");
+},
+after: function () {
+  $('.events').append("<li>after event fired.</li>");
+  }
+  });
+  });
